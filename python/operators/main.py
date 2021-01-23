614469c32b4ac95cb0aaadf8946cfa0f0793d51c
Temirlan <temirlan.balguzhin@bk.ru>
2021-01-23 11:09:32 +0600

    feat(python): add operators

---
+++ python/operators/main.py
@@ -0,0 +1,36 @@
+m = 5
+n = 7
+
+# arithmetic operators
+x = m + n
+x = n - m
+x = m * n
+x = n / m
+x = n ** m
+x = n // m
+x = n % m
+
+# comparison operators
+# x - bool
+x = n < m
+x = n > m
+x = n <= m
+x = n >= m
+x = n == m
+x = n != m
+
+# assignment operators
+x = n
+x += m
+x -= n
+x *= m
+x /= n 
+x **= m
+x //= n 
+x %= n 
+
+# logical operators
+x = n > m and m > 5
+x = n < 5 or m < 5
+x = not n 
+x = n is m
